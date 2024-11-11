c1e13c1a035c36b51407b6bffa6e08408eb830a1
gstechsolutions <gstechsolutions@gmail.com>
2024-11-11 12:13:02 -0600

    Added unit test

--- TempusDevOpsTest/UnitTest1.cs
+++ TempusDevOpsTest/UnitTest1.cs
@@ -5,6 +5,12 @@ namespace TempusDevOpsTest
         [Fact]
         public void Test1()
         {
+            var num1 = 10;
+            var num2 = 20;
+
+            var total = num1 + num2;
+
+            Assert.Equal(30, total);
 
         }
     }
